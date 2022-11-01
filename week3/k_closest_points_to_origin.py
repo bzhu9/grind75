5736258ac30420e4ea3f032b80729413aa13cbe8
Brian Zhu <zhu1013@purdue.edu>
2022-11-01 14:35:03 -0400

    shorter soln k_closest_points_to_origin.py

--- week3/k_closest_points_to_origin.py
+++ week3/k_closest_points_to_origin.py
@@ -1,5 +1,8 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        return sorted(points, key = lambda p : p[0]**2 + p[1]**2)[:k]
+"""class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         dist_p = {}
         for p in points:
             if p[0]**2 + p[1]**2 in dist_p:
@@ -12,4 +15,4 @@ class Solution:
             output.append(dist_p[mins[0]].pop())
             if len(dist_p[mins[0]]) == 0:
                 mins.pop(0)
-        return output
+        return output"""
